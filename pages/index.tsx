911d375fe8d193a156a9b6970bbe5d52059e881c
Jimmy Lin <jimmy.lin@method.com>
2022-12-17 21:22:45 -0800

    feat: added fontfamily and monsterList title

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,13 @@ export default function Home() {
   const color = useStore((state: any) => state.color);
   const changeColor = useStore((state: any) => state.changeColor);
   return (
-    <button className={`text-${color} bg-blue-700 p-2`} onClick={changeColor}>
-      <h1>Default</h1>
-    </button>
+    <div>
+      <h1 className=" text-3xl text-center m-5">
+        DM Monster Screen
+      </h1>
+      <button className={`text-${color} bg-blue-700 p-2`} onClick={changeColor}>
+        <h1>Default</h1>
+      </button>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        serif: [...defaultTheme.fontFamily.serif],
+        mono: [...defaultTheme.fontFamily.mono]
+      }
+    },
   },
   plugins: [],
 }
