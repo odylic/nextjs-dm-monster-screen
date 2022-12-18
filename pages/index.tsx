9f03cef6a28e27b3c90c1268f6a71bf280924b1b
Jimmy Lin <jimmy.lin@method.com>
2022-12-17 18:40:03 -0800

    fix: linting error for deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import useStore from "../store/store";
 
 export default function Home() {
-  const color = useStore((state) => state.color);
-  const changeColor = useStore((state) => state.changeColor);
+  const color = useStore((state: any) => state.color);
+  const changeColor = useStore((state: any) => state.changeColor);
   return (
     <button className={`text-${color} bg-blue-700 p-2`} onClick={changeColor}>
       <h1>Default</h1>
