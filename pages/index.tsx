b04f193ac9f22377c640fbf0388839804e518ad0
Jimmy Lin <jimmy.lin@method.com>
2022-12-17 23:48:27 -0800

    feat: added basic of the monster card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import useStore from "../store/store";
+import MonsterCard from "../src/components/MonsterCard";
 
 export default function Home() {
   const color = useStore((state: any) => state.color);
   const changeColor = useStore((state: any) => state.changeColor);
   return (
-    <div>
-      <h1 className=" text-3xl text-center m-5">
-        DM Monster Screen
-      </h1>
-      <button className={`text-${color} bg-blue-700 p-2`} onClick={changeColor}>
-        <h1>Default</h1>
+    <div className="flex flex-col items-center bg-gray-100 h-screen">
+      <h1 className="text-3xl m-5">DM Monster Screen</h1>
+      <button className="rounded-lg bg-neutral-300 p-4" onClick={changeColor}>
+        Add Monster
       </button>
+      <MonsterCard />
     </div>
   );
 }
---
+++ src/components/MonsterCard.tsx
@@ -0,0 +1,36 @@
+const MonsterCard = () => {
+  return (
+    <div className="bg-gray-200 p-5 rounded-lg m-5 text-center">
+      <div>
+        <input
+          type="text"
+          className="text-center text-xl w-60 h-10 rounded-md"
+          placeholder="Monster Name"
+        />
+      </div>
+      <div className="mt-2">
+        <input
+          type="number"
+          className="w-16 h-10 text-center text-xl rounded-md mr-2"
+          min="0"
+          placeholder="HP"
+        />
+        <input
+          type="number"
+          className="text-center text-xl rounded-md w-16 h-10"
+          placeholder="Temp"
+          min="0"
+        />
+      </div>
+      <div className="m-2">
+        <button className="bg-gray-50 px-2 rounded-md">-</button>
+        <input type="number" className="mx-2 w-24 text-center rounded-md" />
+        <button className="bg-gray-50 px-2 rounded-md">+</button>
+      </div>
+
+      <textarea className="resize" id="text" cols="25" rows="5"></textarea>
+    </div>
+  );
+};
+
+export default MonsterCard;
