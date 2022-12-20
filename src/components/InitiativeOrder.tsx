65f62054780b24fd08887a5d2ee6516fe2ba7ec9
Jimmy Lin <jimmy.lin@method.com>
2022-12-20 12:02:28 -0800

    feat: add visibility functionality

--- src/components/InitiativeOrder.tsx
+++ src/components/InitiativeOrder.tsx
@@ -1,12 +1,48 @@
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const InitiativeOrder = () => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [initiative, setInitiative] = useState("");
+  const [visibility, setVisibility] = useState(false);
+
   return (
-    <div className=" fixed top-0 bg-initiativeBackground h-screen w-[20vw] left-[80vw]">
-      <div>
-        <h1 className="text-xl">Initiative Order</h1>
-      </div>
+    <div>
+      {!visibility && (
+        <div className="fixed top-0 right-0 bg-initiativeBackground text-center">
+          <button
+            className="flex p-3"
+            onClick={(e) => {
+              e.preventDefault();
+              if (visibility) setVisibility(false);
+              if (!visibility) setVisibility(true);
+            }}
+          >
+            <ArrowForwardIosIcon fontSize="large" />
+          </button>
+        </div>
+      )}
+
+      {visibility && (
+        <div className="fixed top-0 bg-initiativeBackground h-screen w-[20vw] left-[80vw]">
+          <div className="">
+            <button
+              className="flex p-3"
+              onClick={(e) => {
+                e.preventDefault();
+                if (visibility) setVisibility(false);
+                if (!visibility) setVisibility(true);
+              }}
+            >
+              <ArrowBackIosIcon fontSize="large" />
+              <h1 className="text-3xl font-serif">Initiative Order</h1>
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
