c5abc72e2538480db4d14d00117a80b46e1f117f
Jimmy Lin <jimmy.lin@method.com>
2022-12-22 12:29:53 -0800

    style: resized

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,9 +34,9 @@ export default function Home() {
         <title>DM Screen</title>
       </Head>
       <h1 className="text-3xl m-5 font-serif">DM Screen</h1>
-      <div className="bg-purple-900 m-5 p-5 rounded-md">
-        <h1 className="text-white">{result}</h1>
-        <form className="flex">
+      <div className="bg-purple-900 m-5 p-5 rounded-md min-w-fit">
+        <h1 className="text-white text-center">{result}</h1>
+        <form className="flex flex-wrap justify-center">
           <input
             className="m-1 rounded-md px-2 w-[60%] min-w-fit"
             placeholder="Enter Dice Notation 4d6 Adv:2d20kh1 Disadvantage:2d20kl1"
@@ -44,28 +44,30 @@ export default function Home() {
               setDice(e.target.value);
             }}
           />
-          <button
-            className="bg-slate-50 active:bg-slate-200 rounded-md px-2 m-1 w-[20%] truncate"
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              roller.roll(dice);
-              let latestRoll = roller.log.shift();
-              setResult(latestRoll + "");
-            }}
-          >
-            Roll
-          </button>
-          <button
-            className="bg-slate-50 active:bg-slate-200 rounded-md px-2 m-1 w-[20%] truncate"
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              setResult("");
-            }}
-          >
-            Clear
-          </button>
+          <div>
+            <button
+              className="bg-slate-50 active:bg-slate-200 rounded-md px-2 m-1 w-[20%] min-w-fit"
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault();
+                roller.roll(dice);
+                let latestRoll = roller.log.shift();
+                setResult(latestRoll + "");
+              }}
+            >
+              Roll
+            </button>
+            <button
+              className="bg-slate-50 active:bg-slate-200 rounded-md px-2 m-1 w-[20%] min-w-fit"
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault();
+                setResult("");
+              }}
+            >
+              Reset
+            </button>
+          </div>
         </form>
       </div>
       <button
